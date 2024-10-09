70076e96734df9ce64761d1341421f6e2a77cf0a
phamdinhthanhcntt6 <90460582+phamdinhthanhcntt6@users.noreply.github.com>
2024-10-09 17:27:33 +0700

    thanh fix ui

--- lich-nhu-y/src/components/IconZodiac/index.tsx
+++ lich-nhu-y/src/components/IconZodiac/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const IconZodiac = () => {
   return (
-    <div className="mb-[38px] flex gap-4 overflow-x-auto flex-nowrap hide-scrollbar">
+    <div className="mb-[38px] flex gap-4 overflow-x-auto flex-nowrap hide-scrollbar cursor-pointer">
       {iconZodiacData.map((item, index) => (
         <Image alt={`${item.id}`} src={item.image} key={index} />
       ))}
